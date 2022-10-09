0c0a416b8c2e8f8adec3f7920c17396da6b3cd0a
Julius-Taslim <113356526+Julius-Taslim@users.noreply.github.com>
2022-10-09 15:26:12 +0700

    Add files via upload

---
+++ soalno2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int angka;
+
+  cout <<"Program perulangan ascending."<<endl;
+  
+  for(angka = 1; angka <= 10; angka++)
+    cout <<angka<<endl;
+
+  cout <<endl;
+  
+  cout <<"Program perulangan descending."<<endl;
+
+  for(angka = 10; angka >=1; angka--)
+    cout <<angka<<endl;
+
+  return 0;
+  
+}
